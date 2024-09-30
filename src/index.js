4daec303775d9f81c735e9ebe5b3e11896cb96d3
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2024-09-29 22:28:04 -0400

    trying to see why my repository is showing 404

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { connect } = require("./db/init");
 const app = express();
 const PORT = process.env.PORT || 3000;
+//comment
 connect()
   .then(() => {
     app.listen(PORT, () => {
